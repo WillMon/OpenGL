4a9535f13b370d16b1a21e0624d328d77e3b00ea
WillMon <w.montero34@gmail.com>
2016-08-31 13:12:32 -0500

    Will Mon 3rd Commit 03/31/2016
    Placed What made a planet in the constructer

--- OpenGL/Planets.h
+++ OpenGL/Planets.h
@@ -8,19 +8,24 @@ class Planet
 {
 public:
 	Planet(vec3,mat4, int, int ,int);
-	~Planet();
+	~Planet();	
+	
 
 private:
-	
 	vec3 Location;
 	mat4 matrix;
 	int radius, row, columes;
+
 	
 };
 
-Planet::Planet(vec3,mat4,int,int,int)
+Planet::Planet(vec3 vec ,mat4 mat ,int rad,int r,int col)
 {
-
+	Location = vec;
+	matrix = mat;
+	radius = rad;
+	row = r;
+	columes = col;
 }
 
 Planet::~Planet()
